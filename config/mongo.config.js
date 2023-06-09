b3eb56d11e9a03cb1a1b848aec160762ab1f8b55
SynFunjioka <alanff2800@gmail.com>
2023-06-09 01:07:11 -0600

    New package: Dotenv. This package is used to manage environment variables and use them. Implemented in mongo.config.js

--- config/mongo.config.js
+++ config/mongo.config.js
@@ -1,5 +1,9 @@
+require("dotenv").config();
+
+const { MONGO_HOST, MONGO_PORT, MONGO_DB } = process.env;
+
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/InexApp",
+  mongoURI: `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`,
   options: {
     useNewUrlParser: true, //* Utilizar el nuevo motor de análisis de URL
     useUnifiedTopology: true, //* Utilizar el nuevo motor de topología
